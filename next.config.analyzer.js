e9ecaa8f348f892064924d72ef66737e825774cd
technicalJin <varunjinjala@gmail.com>
2025-11-11 17:49:45 +0530

    Updated

---
+++ next.config.analyzer.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/**
+ * Next.js Configuration with Bundle Analyzer
+ * Use: ANALYZE=true npm run build
+ */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: true,
+});
+
+const nextConfig = require('./next.config');
+
+module.exports = withBundleAnalyzer(nextConfig);
